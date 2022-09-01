6b9497b66b930bb10d85a804bb8d8b882961e650
Shape Lee <shape1026@gmail.com>
2022-09-02 00:11:33 +0900

    [220901] 서브페이지 보고서 다운 수정

--- src/component/sustainability/Report.js
+++ src/component/sustainability/Report.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
-import Button from '../Button';
+import React from "react";
+import styled from "styled-components";
+import Button from "../Button";
 
-import img from '../../assets/images/subPage/img1.jpg'
+import img from "../../assets/images/subPage/img1.jpg";
 
 const StyledSection = styled("section")`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 80px 0; 
-  padding: 100px 24px;
+  margin: 80px 0;
+  padding: 40px 24px;
   background: url(${img}) no-repeat 50% / cover;
 
-  @media ${props => props.theme.desktop} {
+  @media ${(props) => props.theme.desktop} {
     margin: 100px 0;
 
     padding: 150px 0;
@@ -20,7 +20,7 @@ const StyledSection = styled("section")`
 `;
 
 const StyledWrap = styled("div")`
-  padding: 40px 100px;
+  padding: 40px;
   width: 90%;
   max-width: 1400px;
   text-align: center;
@@ -39,15 +39,17 @@ const StyledWrap = styled("div")`
   .block {
     display: block;
   }
-`;  
-
+`;
 
 const Report = () => {
   return (
     <StyledSection>
       <StyledWrap>
         <h2>HP 보고서</h2>
-        <p>HP 2021 지속 가능한 영향 보고서가 여기에 있습니다.<span className='block'>지금 확인하세요.</span></p>
+        <p>
+          HP 2021 지속 가능한 영향 보고서가 여기에 있습니다.
+          <span className="block">지금 확인하세요.</span>
+        </p>
         <Button title={"보고서 다운로드"} bg={"black"} text={"white"} />
       </StyledWrap>
     </StyledSection>
